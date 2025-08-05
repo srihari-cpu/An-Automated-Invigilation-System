ac2a3047afbd185860d1c2359565536a52048808
Srihari <srihariyadav1343@gmail.com>
2025-08-05 15:27:27 +0530

    result corrected

--- result.js
+++ result.js
@@ -12,13 +12,11 @@ window.onload = function () {
       ).toLocaleDateString();
       row.insertCell(1).textContent = assignment.session;
       row.insertCell(2).textContent = assignment.block;
-      row.insertCell(3).textContent = assignment.hall;
-      row.insertCell(4).textContent = assignment.floor;
-      row.insertCell(5).textContent = assignment.faculty;
+      row.insertCell(3).textContent = assignment.faculty;
     });
   } else {
     const row = assignmentTable.insertRow();
     row.insertCell(0).textContent = "No Assignments Found";
-    for (let i = 1; i < 6; i++) row.insertCell(i).textContent = "";
+    for (let i = 1; i < 4; i++) row.insertCell(i).textContent = "";
   }
 };
